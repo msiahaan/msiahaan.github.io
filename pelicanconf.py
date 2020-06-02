08a014e3dfba46996e7c2faa561d800fd1729903
msiahaan <mico.siahaan@gmail.com>
2020-06-02 20:03:40 +0800

    Mofified pelicancof.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,11 +23,11 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/bangmico'),
+          ('Facebook', 'https://facebook.com'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -35,4 +35,4 @@ DEFAULT_PAGINATION = 10
 #RELATIVE_URLS = True
 
 #Use other theme
-THEME = '/home/micosiahaan/staticwebsite/websites/pelican-themes/blueidea'
+THEME = '/home/micosiahaan/staticwebsite/lib/python3.8/site-packages/pelican/themes/notmyidea'
